e82d709693ee8918ab7a56e5f5e77832db62c3e7
arun <crcravi66@gmail.com>
2024-10-13 16:37:26 +0530

    added for increment decrement conditions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter)
+
+  const increamentHandler = () => {
+    dispatch({ type: "increment" })
+  }
+  const increaseHendler = () => {
+    dispatch({ type: "increase", amount: 10 })
+  }
+  const decreamentHandler = () => {
+    dispatch({ type: "decrement" })
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={increamentHandler}>Increment</button>
+        <button onClick={increaseHendler}>Increase by 10</button>
+        <button onClick={decreamentHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
